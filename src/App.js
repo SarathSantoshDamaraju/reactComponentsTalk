39fdb338ffe31156faf0c12b488b79e5ceace83b
Sarath Damaraju <sarathdamaraju@hotmail.com>
2017-06-23 20:23:44 +0530

    call back done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
+    constructor () {
+        super();
+        this.state = {
+            data :  ''
+        }
+        this.getData = this.getData.bind(this);
+        this.returnData = this.returnData.bind(this);
+    }
+
+getData(event) {
+    this.setState({
+        data: event.target.value
+    })
+}
+
+returnData(event) {
+    event.preventDefault();
+    this.props.callBack(this.state.data);
+}
+
+render() {
+    return(
+        <form onSubmit={this.returnData}>
+        <input type="text" onChange={this.getData} placeholder="Enter Image URL" />
+        <input type="Submit" value="Show Image"/>
+        </form>
+    );
+}
 
 }
 
+
+App.protoTypes = {
+callBack : PropTypes.func,
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,25 @@ import App from './App';
 import './index.css';
 
 class Index extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      url : ''
+    }
+  }
 
- 
+showData(params){
+this.setState({url : params});
+}
+
+ render(){
+   return (
+     <div>
+     <App callBack={this.showData.bind(this)} />
+     {this.state.url !== '' ? <img src={this.state.url} /> : <p>No Image Loaded</p>}
+     </div>
+   );
+ }
 }
 
 ReactDOM.render(
